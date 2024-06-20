b5312d6edcdeda9e2835821feda47d92ebb30686
P5V1V3K <113207270+P5V1V3K@users.noreply.github.com>
2024-06-20 19:22:44 +0000

    pdf upload

--- app.py
+++ app.py
@@ -1,12 +1,49 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'supersecretkey'  # This should be set to a secure random value in production
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Slayer'
+def home():
+    return render_template('home.html')
 
+@app.route('/download_file/<filename>')
+def download_file(filename):
+    return "uploaded %s" %filename
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(url_for('home'))
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(url_for('home'))
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('file uploaded')
+            return redirect(url_for('home'))
+    return
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
